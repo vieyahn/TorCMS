5a506ed1bef4860841326b951f6741914a834826
Bu Kun <bukun@osgeo.cn>
2016-05-10 22:32:12 +0800

    Update mcatalog.py

--- src/torcms/model/mcatalog.py
+++ src/torcms/model/mcatalog.py
@@ -40,6 +40,9 @@ class MCatalog(MSingleTable):
     def query_field_count(self, limit_num):
         return self.tab.select().order_by(self.tab.count.desc()).limit(limit_num)
 
+    def get_by_uid(self, slug):
+        return self.tab.get(uid = uid)
+        
     def get_by_slug(self, slug):
         return self.tab.get(slug=slug)
 
