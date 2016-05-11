15abc84c949382b9be6b0e95013c91b41cdf44bd
bukun <bukun@osgeo.cn>
2016-05-11 22:41:00 +0800

    update the version to 0.1.0 .

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='torcms',
-    version='0.0.9',
+    version='0.1.0',
     keywords=('torcms', 'tornado', 'cms',),
     description='''CMS based on Python 3 and Tornado.
         Flexible, extensible web CMS framework built on Tornado, Peewee and Purecss, compatible with Python 3.4 and 3.5.
