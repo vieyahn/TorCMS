07c8947ce8b84e54aa72dbabf7c6bf3bfa102c8f
bukun <bukun@osgeo.cn>
2016-05-12 11:23:56 +0800

    fix a bug.

--- src/torcms/model/app_model.py
+++ src/torcms/model/app_model.py
@@ -179,7 +179,7 @@ class MApp(MAppBase):
         # condition = {'keywords': {'$elemMatch': {'$eq': tag_name}}}
         condition = {'keywords': [tag_name]}
 
-        return self.tab_app.select().where(self.tab_app.extinfo.contains(condition)).order_by(self.tab_app.updatetime.desc())
+        return self.tab_app.select().where(self.tab_app.extinfo.contains(condition)).order_by(self.tab_app.time_update.desc())
 
     def get_label_fenye(self, tag_slug, page_num):
         all_list = self.query_by_tagname(tag_slug)
