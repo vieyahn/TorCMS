23b7a92ceff7c8fb7aa97ae272e813b6cc12a365
bukun <bukun@osgeo.cn>
2016-08-13 16:11:30 +0800

    去掉whoosh工具中冗余的代码。

--- src/torcms/core/tool/whoosh_tool.py
+++ src/torcms/core/tool/whoosh_tool.py
@@ -1,41 +1,50 @@
 # -*- coding:utf-8 -*-
 
 from whoosh.index import open_dir
-from jieba.analyse import ChineseAnalyzer
+# from jieba.analyse import ChineseAnalyzer
 from whoosh.qparser import QueryParser
 from whoosh.query import *
 
-analyzer = ChineseAnalyzer()
-ix = open_dir("database/whoosh")
-parser = QueryParser("content", schema=ix.schema)
-
-
+# analyzer = ChineseAnalyzer()
+def singleton(cls, *args, **kwargs):
+    instances = {}
+    def _singleton():
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return _singleton
+@singleton
 class yunsearch():
+    def __init__(self):
+        self.ix = open_dir("database/whoosh")
+        self.parser = QueryParser("content", schema=self.ix.schema)
+
     def get_all_num(self, keyword, catid=''):
-        q = parser.parse(keyword)
+        q = self.parser.parse(keyword)
         if catid == '':
             pass
         else:
             q = And([Term("catid", catid), q])
-        return len(ix.searcher().search(q).docs())
+        return len(self.ix.searcher().search(q).docs())
 
     def search(self, keyword, limit=20):
-        q = parser.parse(keyword)
+        q = self.parser.parse(keyword)
         try:
-            tt = ix.searcher().search(q, limit=limit)
+            tt = self.ix.searcher().search(q, limit=limit)
             return (tt)
         finally:
             pass
 
     def search_pager(self, keyword, catid='', page_index=1, doc_per_page=10):
 
-        q = parser.parse(keyword)
+        q = self.parser.parse(keyword)
         if catid == '':
             pass
         else:
             q = And([Term("catid", catid), q])
         try:
-            tt = ix.searcher().search(q, limit=page_index * doc_per_page)
+            tt = self.ix.searcher().search(q, limit=page_index * doc_per_page)
             return (tt[(page_index - 1) * doc_per_page: page_index * doc_per_page])
         finally:
             pass
+
